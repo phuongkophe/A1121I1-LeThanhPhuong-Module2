56696fae869bfd58056af77c49a2791bcc88a754
phuongkophe <phuong230602@gmail.com>
2022-02-18 21:25:18 +0700

    add bai tap

---
+++ src/ss1_java_overview/thuc_hanh/Linear_equation_resolver.java
@@ -0,0 +1,30 @@
+package ss1_java_overview.thuc_hanh;
+
+import java.util.Scanner;
+
+public class Linear_equation_resolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextFloat();
+
+        System.out.println("b: ");
+        double b = scanner.nextFloat();
+
+        System.out.println("c: ");
+        double c = scanner.nextFloat();
+
+        if (a!=0){
+            double answer = (c-b)/a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        }else {
+            if (b == c) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
