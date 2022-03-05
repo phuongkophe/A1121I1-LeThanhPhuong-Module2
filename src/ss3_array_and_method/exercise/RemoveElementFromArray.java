00ab2928d91a647a5597c9009d5c1c438e627706
phuongkophe <phuong230602@gmail.com>
2022-03-05 23:48:34 +0700

    add bai tap

--- src/ss3_array_and_method/exercise/RemoveElementFromArray.java
+++ src/ss3_array_and_method/exercise/RemoveElementFromArray.java
@@ -1,35 +1,34 @@
 package ss3_array_and_method.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RemoveElementFromArray {
     public static void main(String[] args) {
-        int[] array = {2, 4, 6, 7, 8, 10};
-        int index = -1;
-        int x;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the element you want to remove from the array: ");
-        x = sc.nextInt();
-        for (int i = 0; i < array.length; i++) {
-            if (x == array[i]) {
-                index = i;
-                System.out.println("Element " + x + " has index " + index + " in the array");
-                break;
-            }
-            if (index != -1) {
-                for (int j = index; j < array.length - 1; j++) {
-                    array[j] = array[j + 1];
-                    System.out.println("Array after remove " + x);
-                    for ( i = 0; i < array.length - 1; i++) {
-                        System.out.println(array[i] + " ");
-                    }
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
+//        System.out.println("Thoi gian bat dau: " + stopWatch.getStartTime());
+        Integer[] arr = {2, 1, 2, 2, 3, 4};
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập phần tử cần xóa: ");
+        Integer itemDelete = scanner.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == itemDelete) {
+                count++;
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
                 }
-            } else {
-                System.out.println("Not found in array");
-                break;
+                i--;
+//                arr[arr.length - count] = 0;
             }
-
-
         }
+        for (int i = 1; i <= count; i++) {
+            arr[arr.length - i] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
+//        stopWatch.stop();
+//        System.out.println("Thoi gian ket thuc: " + stopWatch.getEndTime());
+//        System.out.println("Thoi gian chenh lech: " + stopWatch.getElapsedTime());
     }
 }
