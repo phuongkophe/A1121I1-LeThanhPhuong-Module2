84e5e6f18dcb5112ba0824416deb1a32be010014
phuongkophe <phuong230602@gmail.com>
2022-03-23 21:18:25 +0700

    update bt

---
+++ src/ss13_searching_algorithm/exercise/search_ascending_string/Main.java
@@ -0,0 +1,21 @@
+package ss13_searching_algorithm.exercise.search_ascending_string;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = scanner.nextLine();
+        List<Character> max = new ArrayList<>();
+
+        for (int i=0; i<str.length();i++){
+            List<Character> list = new ArrayList<>();
+            
+        }
+    }
+
+}
